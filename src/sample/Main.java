f0e1c212863d380b681faceb89f425693a8ba039
Adam Tomczak <atomczak30@gmail.com>
2018-10-04 11:41:45 +0200

    Teraz to już jest naprawde niezle xD

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,24 +10,28 @@ import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Zmiana wielkości liter");
 
+        primaryStage.setTitle("Zmiana wielkości liter");
         GridPane pane = new GridPane();
         pane.setPadding(new Insets(10,10,10,10));
         pane.setHgap(5);
         pane.setVgap(5);
 
         ColumnConstraints col = new ColumnConstraints();
-        col.setPercentWidth(40);
+        col.setMaxWidth(300);
         ColumnConstraints col2 = new ColumnConstraints();
         col2.setPercentWidth(60);
         pane.getColumnConstraints().addAll(col,col2);
+
+        //////TEXT FIELDS AND LABELS
+        GridPane leftPane = new GridPane();
 
         Label zr = new Label("Źródło");
         GridPane.setConstraints(zr,0,0);
@@ -42,6 +46,12 @@ public class Main extends Application {
         TextArea p = new TextArea("OLA");
         GridPane.setConstraints(p,0,3);
 
+        leftPane.getChildren().addAll(zr,t,wynik,p);
+
+        ////////////BUTTONS
+
+        VBox rightPane = new VBox();
+
         Button but1 = new Button("Przetwarzaj!");
         GridPane.setConstraints(but1,1,0);
         Button but2 = new Button("Wyczyść Źródło");
@@ -53,14 +63,11 @@ public class Main extends Application {
         Button but5 = new Button("Zapisz i zakończ");
         GridPane.setConstraints(but5,1,4);
 
-        /*but1.setAlignment(Pos.TOP_RIGHT);
-        but2.setAlignment(Pos.CENTER_RIGHT);
-        but3.setAlignment(Pos.CENTER_RIGHT);
-        but4.setAlignment(Pos.BASELINE_RIGHT);
-        but5.setAlignment(Pos.BOTTOM_RIGHT);*/
+        rightPane.getChildren().addAll(but1,but2,but3,but4,but5);
 
-        zr.setPrefHeight(15);
-        wynik.setPrefHeight(5);
+       // zr.setPrefHeight(15);
+       // wynik.setPrefHeight(5);
+
 
         but1.setMaxWidth(Double.MAX_VALUE);
         but2.setMaxWidth(Double.MAX_VALUE);
@@ -68,17 +75,16 @@ public class Main extends Application {
         but4.setMaxWidth(Double.MAX_VALUE);
         but5.setMaxWidth(Double.MAX_VALUE);
 
-        but1.setPrefHeight(58);
-        but2.setPrefHeight(58);
-        but3.setPrefHeight(58);
-        but4.setPrefHeight(58);
-        but5.setPrefHeight(58);
+        but1.setPrefHeight(68);
+        but2.setPrefHeight(68);
+        but3.setPrefHeight(68);
+        but4.setPrefHeight(68);
+        but5.setPrefHeight(68);
 
 
-       // wynik.setPrefHeight(90);
+        pane.add(rightPane,1,0);
+        pane.add(leftPane,0,0);
 
-
-        pane.getChildren().addAll(zr,t,wynik,p,but1,but2,but3,but4,but5);
         Scene scene = new Scene(pane,550,430);
 
         primaryStage.setScene(scene);
